64accdea934299e31e87cb559b6c4ee6be546387
Aladar <aladarmiao1@gmail.com>
2020-07-05 22:43:27 -0700

    deploy

---
+++ Actuary/build/precache-manifest.c038e5263260431cb01dce5aa6f891df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e282f46fb39c0d59c4bc255ebd3e7dc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e53b4a4805cb524dab3",
+    "url": "/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "6f3ac0483943476463c6",
+    "url": "/static/css/main.70bf51a6.chunk.css"
+  },
+  {
+    "revision": "6e53b4a4805cb524dab3",
+    "url": "/static/js/2.af5fbe8a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.af5fbe8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f3ac0483943476463c6",
+    "url": "/static/js/main.7bb2689b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
