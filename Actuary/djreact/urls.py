6f0f4367e45b0fdf887ffc434da8f0c8c10ce9d8
Aladar <aladarmiao1@gmail.com>
2020-07-05 23:35:25 -0700

    deploy

--- Actuary/djreact/urls.py
+++ Actuary/djreact/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', admin.site.urls),
+    
     path('api/', include('articles.api.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
